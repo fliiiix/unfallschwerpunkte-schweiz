4beee67737d5101dc03c7d286af91d7644d6d198
Felix <hi@l33t.name>
2019-11-17 16:46:58 +0100

    Basic d3 function

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Unfallschwerpunkte der Schweiz</h1>
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home,
   }
 }
 </script>
